30167c7768075112d68a7c90d5185f3b2ba4397f
wsj1122 <3306852830@qq.com>
2021-04-26 11:10:49 +0800

    第11章例题

---
+++ homework6/src/homework6/CastingDemo.java
@@ -0,0 +1,26 @@
+package homework6;
+
+public class CastingDemo {
+	public static void main(String[] args) {
+		Object object1 = new Circle(1);
+		Object object2 = new Rectangle(1,1);
+		
+		displayObject(object1);
+		displayObject(object2);
+	}
+	
+	public static void displayObject(Object object) {
+		if (object instanceof Circle) {
+			System.out.println("The circle area is" +
+		((Circle)object).getArea());
+			System.out.println("The circle diameter is" +
+					((Circle)object).getDiameter());
+		}
+		else if(object instanceof Rectangle) {
+			System.out.println("The Rectangle area is" +
+					((Rectangle)object).getArea());;
+			
+		}
+	}
+
+}
---
+++ homework6/src/homework6/TestCircleRectangle.java
@@ -0,0 +1,18 @@
+package homework6;
+
+public class TestCircleRectangle {
+	public static void main(String[] args) {
+		Circle circle = new Circle(1);
+		System.out.println("A circle " + circle.toString());
+		System.out.println("The color is " + circle.getColor());
+		System.out.println("The radius is" + circle.getRadius());
+		System.out.println("The area is " + circle.getArea());
+		System.out.println("The diameter is" + circle.getDiameter());
+		
+		Rectangle rectangel = new Rectangle(2,4);
+		System.out.println("\nA rectangel " + rectangel.toString());
+		System.out.println("The area is " + rectangel.getArea());
+		System.out.println("The perimeter is" + rectangel.getPerimeter());
+	
+	}
+}
